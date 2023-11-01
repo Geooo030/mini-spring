6c6c31fefc7c4dd67c57f434b71e6dba21285df3
wangzq <wangzq@akulaku.com>
2023-11-01 16:16:31 +0800

    测试 git cherry-pick 功能1

---
+++ src/test/java/org/springframework/test/git/CherryPickFun1.java
@@ -0,0 +1,12 @@
+package org.springframework.test.git;
+
+/**
+ * @author wangzq
+ * @date 2023/11/1 16:15
+ */
+public class CherryPickFun1 {
+
+    public static void main(String[] args) {
+        System.out.println("I am function A's commit code");
+    }
+}
