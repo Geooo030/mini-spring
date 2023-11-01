bbaf9ed3f495069e9faada4eec87e4aca79ad8a0
wangzq <wangzq@akulaku.com>
2023-11-01 16:19:26 +0800

    测试 git cherry-pick 功能2

---
+++ src/test/java/org/springframework/test/git/CherryPickFunc2.java
@@ -0,0 +1,12 @@
+package org.springframework.test.git;
+
+/**
+ * @author wangzq
+ * @date 2023/11/1 16:17
+ */
+public class CherryPickFunc2 {
+
+    public static void main(String[] args) {
+        System.out.println("I am test cherry pick fuction2, I have merge cherry-pick-test. But Im testing periods.");
+    }
+}
